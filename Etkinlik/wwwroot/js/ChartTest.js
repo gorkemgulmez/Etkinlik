567a48f53ae43353b1e788be56f2d33bf240ac78
gorkemgulmez <gorkemgulmez7@gmail.com>
2018-07-26 17:31:21 +0300

    Ajax get post methotları başlangıç

---
+++ Etkinlik/wwwroot/js/ChartTest.js
@@ -0,0 +1,17 @@
+﻿var ctx = document.getElementById("myChart").getContext('2d');
+var mychart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ["Anket"],
+        datasets: [{
+            label: 'Oy Sayısı',
+            data: [1],
+            
+            borderWidth: 1
+        }]
+    },options:{
+        legend: {
+            display: false
+        }
+    }
+});
---
+++ Etkinlik/wwwroot/js/SurveyIndex.js
@@ -0,0 +1,16 @@
+﻿
+function getData(id) {
+    $.ajax({
+        type: 'GET',
+        url: '/testi',
+        data: { id: id },
+        dataType: "json"
+        success: dataRender
+    }).done(function () {
+        alert("Oldu");
+    });
+}
+
+function dataRender(data) {
+    alert(data);
+}
